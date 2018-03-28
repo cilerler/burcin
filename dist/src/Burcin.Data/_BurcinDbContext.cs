a32869a8b78cb1d9103f9aac59b5ff5fe964b489
Cengiz Ilerler <cilerler@gmail.com>
2018-03-28 11:07:56 -0400

    minor improvement on instructions

--- dist/src/Burcin.Data/_BurcinDbContext.cs
+++ dist/src/Burcin.Data/_BurcinDbContext.cs
@@ -3,12 +3,10 @@ using Burcin.Models;
 
 namespace Burcin.Data
 {
-    public sealed class BurcinDbContext : DbContext
+    public partial class BurcinDbContext : DbContext
     {
         public BurcinDbContext(DbContextOptions options) : base(options)
         {
         }
-
-        //public DbSet<Burcin> Burcins { get; set; }
     }
 }
--- dist/tools/EntityFramework/scaffold.ps1
+++ dist/tools/EntityFramework/scaffold.ps1
@@ -1,10 +1,9 @@
-dotnet ef dbcontext scaffold "data source=localhost;initial catalog=(databaseName);Trusted_Connection=True;MultipleActiveResultSets=True;App=Burcin" Microsoft.EntityFrameworkCore.SqlServer -f -d -o "..\Burcin.Models\(databaseName)" -c "BurcinDbContext" --use-database-names --schema MySchema -t MySchema.MyTable1 -t MySchema.MyTable2
+dotnet ef dbcontext scaffold "data source=localhost;initial catalog=(databaseName);Trusted_Connection=True;MultipleActiveResultSets=True;App=Burcin" Microsoft.EntityFrameworkCore.SqlServer -f -d -o "..\Burcin.Models\(databaseName)" -c "BurcinDbContext" --schema MySchema -t MySchema.MyTable1 -t MySchema.MyTable2
 
-# 1. Rename `namespace Burcin.Console` to `namespace Burcin.Models.(databaseName)` in directory `..\Burcin.Models\(databaseName)`
+# 1. Rename all `namespace Burcin.Console` to `namespace Burcin.Models.(databaseName)` in directory `..\Burcin.Models\(databaseName)`
 # 2. locate `BurcinDbContext.cs` file in directory `..\Burcin.Models\(databaseName)`
 #   a. move it into `Burcin.Data` project
-#   b. locate and open the file
+#   b. locate and open the file again
 #   c. rename `namespace Burcin.Models.(databaseName)` to `namespace Burcin.Data`
 #   d. add `using Burcin.Models.(databaseName);` to the top
-#   e. add `public BurcinDbContext(DbContextOptions options) : base(options) {}`
-#   f. clean #warning pragma & server connection string under `protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)`
+#   e. clean #warning pragma & server connection string under `protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)`
