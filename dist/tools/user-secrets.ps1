618dd3de5df657b7b8cd27d7a740f00083ea7117
Cengiz Ilerler <cilerler@gmail.com>
2018-04-13 11:11:25 -0400

    updated connection string syntax

--- dist/tools/user-secrets.ps1
+++ dist/tools/user-secrets.ps1
@@ -4,4 +4,4 @@
 Set-Location ".\src\Burcin.Console";
 dotnet user-secrets set ConnectionStrings:DefaultConnection "data source=localhost;initial catalog=(databaseName);Trusted_Connection=True;MultipleActiveResultSets=True;App=Burcin.Console"
 
-dotnet user-secrets --project "..\Burcin.Api" set ConnectionStrings:DefaultConnection "Server=(localdb)\\mssqllocaldb;Database=(databaseName);Trusted_Connection=True;MultipleActiveResultSets=true;App=Burcin.Api"
+dotnet user-secrets --project "..\Burcin.Api" set ConnectionStrings:DefaultConnection "data source=localhost;initial catalog=(databaseName);Trusted_Connection=True;MultipleActiveResultSets=true;App=Burcin.Api"
