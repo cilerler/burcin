cf30beaf68f105b64cea4fad51e060c4451b94ac
Cengiz Ilerler <cilerler@gmail.com>
2018-04-23 08:19:26 -0400

    improved sample

--- dist/src/Burcin.Data/_BurcinDbContext.cs
+++ dist/src/Burcin.Data/_BurcinDbContext.cs
@@ -10,3 +10,22 @@ namespace Burcin.Data
         }
     }
 }
+
+// namespace Burcin.Data
+// {
+//     public partial class BurcinDbContext : DbContext
+//     {
+//         public virtual DbSet<T> T { get; set; }
+
+//         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//         {
+//             if (!optionsBuilder.IsConfigured)
+//             {
+//             }
+//         }
+
+//         protected override void OnModelCreating(ModelBuilder modelBuilder)
+//         {
+//         }
+//     }
+// }
