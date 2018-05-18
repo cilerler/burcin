026c6edfa2365caa5ecb0cb3954705651c061c46
Cengiz Ilerler <cilerler@gmail.com>
2018-05-18 10:07:27 -0400

    added IOptions examples

--- dist/src/Burcin.Domain/Helper.cs
+++ dist/src/Burcin.Domain/Helper.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+#if (EntityFramework)
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+#endif
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -13,12 +16,16 @@ namespace Burcin.Domain
         private readonly IServiceProvider _serviceProvider;
         private readonly ILogger _logger;
         private readonly HelperSetting _options;
+        private readonly HelperSetting _optionsSnapshot;
+        private readonly HelperSetting _optionsMonitor;
 
-        public Helper(IServiceProvider serviceProvider, ILogger<Helper> logger, IOptionsSnapshot<HelperSetting> options)
+        public Helper(IServiceProvider serviceProvider, ILogger<Helper> logger, IOptions<HelperSetting> options, IOptionsSnapshot<HelperSetting> optionsSnapshot, IOptionsMonitor<HelperSetting> optionsMonitor)
         {
             _serviceProvider = serviceProvider;
             _logger = logger;
             _options = options.Value;
+            _optionsSnapshot = optionsSnapshot.Value;
+            _optionsMonitor = optionsMonitor.CurrentValue;
         }
 
         public string Echo(string input)
@@ -28,39 +35,43 @@ namespace Burcin.Domain
                 throw new ArgumentNullException();
             }
 
-            _logger.LogInformation("Echo requested, input {value}", input);
-            return $"{_options.Prefix} {input}";
+            _logger.LogInformation("Echo requested. {Value} {Options}", input, _options.Prefix);
+	        _logger.LogInformation("Echo requested. {Value} {OptionsSnapShot}", input, _optionsSnapshot.Prefix);
+	        _logger.LogInformation("Echo requested. {Value} {OptionsMonitor}", input, _optionsMonitor.Prefix);
+
+
+			return $"{_options.Prefix} {input}";
         }
 
         public void DoWork()
         {
-            //! Uncomment block below if you are using EntityFramework
-            //var scopeFactory = _serviceProvider.GetRequiredService<IServiceScopeFactory>();
-            //using (var scope = scopeFactory.CreateScope())
-            //    using (var context = scope.ServiceProvider.GetRequiredService<Burcin.Data.BurcinDbContext>())
-            //    {
-                    // try
-                    // {
-                    //     _logger.LogInformation("Saving changes");
-                    //     context.SaveChanges();
-                    // }
-                    // catch (Microsoft.EntityFrameworkCore.Storage.RetryLimitExceededException)
-                    // {
-                    //     _logger.LogWarning("Unable to save changes. Try again, and if the problem persists, see your system administrator.");
-                    // }
-                    // catch (Microsoft.EntityFrameworkCore.DbUpdateException due)
-                    // {
-                    //     foreach (EntityEntry entry in due.Entries)
-                    //     {
-                    //         _logger.LogError(due, $"Entity of type {entry.Entity.GetType().Name} in state {entry.State} has the following validation errors:");
-                    //     }
-                    // }
-                    // catch (System.Exception)
-                    // {
-
-                    //     throw;
-                    // }
-            //    }
+#if (EntityFramework)
+            var scopeFactory = _serviceProvider.GetRequiredService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+            using (var context = scope.ServiceProvider.GetRequiredService<Burcin.Data.BurcinDbContext>())
+            {
+                try
+                {
+                    _logger.LogInformation("Saving changes");
+                    context.SaveChanges();
+                }
+                catch (Microsoft.EntityFrameworkCore.Storage.RetryLimitExceededException)
+                {
+                    _logger.LogWarning("Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+                catch (Microsoft.EntityFrameworkCore.DbUpdateException due)
+                {
+                    foreach (EntityEntry entry in due.Entries)
+                    {
+                        _logger.LogError(due, $"Entity of type {entry.Entity.GetType().Name} in state {entry.State} has the following validation errors:");
+                    }
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+            }
+#endif
         }
     }
 }
