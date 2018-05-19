22d52df3137001a229f8fca8597a651a41391baf
Cengiz Ilerler <cilerler@gmail.com>
2018-05-19 17:32:59 -0400

    Update build.ps1

--- tools/build.ps1
+++ tools/build.ps1
@@ -7,13 +7,8 @@ if(Test-Path ..\artifacts) {
 	Remove-Item ..\artifacts -Force -Recurse
 }
 
-Write-Host "build: Version branch is $env:APPVEYOR_REPO_BRANCH"
-Write-Host "build: Version id is $env:APPVEYOR_BUILD_ID"
-Write-Host "build: Version revision is $env:APPVEYOR_BUILD_NUMBER"
-Write-Host "build: Version version is $env:APPVEYOR_BUILD_VERSION"
-
 Write-Host "build: Attempting to pack file..."
-	& nuget pack ..\burcin.nuspec -NonInteractive -OutputDirectory ..\artifacts -Verbosity Detailed -version 1.0.473.1
+	& nuget pack ..\burcin.nuspec -NonInteractive -OutputDirectory ..\artifacts -Verbosity Detailed -version $env:APPVEYOR_BUILD_VERSION
     if($LASTEXITCODE -ne 0) { exit 1 }
 
 Pop-Location
