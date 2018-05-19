9af5bac3364a7fd39d1607ba3d86bd7534566291
Cengiz Ilerler <cilerler@gmail.com>
2018-05-19 16:53:12 -0400

    Update build.ps1

--- tools/build.ps1
+++ tools/build.ps1
@@ -2,9 +2,9 @@ echo "build: Build started"
 
 Push-Location $PSScriptRoot
 
-if(Test-Path .\artifacts) {
-	echo "build: Cleaning .\artifacts"
-	Remove-Item .\artifacts -Force -Recurse
+if(Test-Path ..\artifacts) {
+	echo "build: Cleaning ..\artifacts"
+	Remove-Item ..\artifacts -Force -Recurse
 }
 
 $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
@@ -12,6 +12,8 @@ $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUI
 $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 
 echo "build: Version suffix is $suffix"
+echo "build: Version suffix is $revision"
+echo "build: Version suffix is $branch"
 
 echo "Attempting to pack file: burcin.nuspec"
 	& nuget pack ..\burcin.nuspec -NonInteractive -OutputDirectory ..\artifacts -Verbosity Detailed -version 1.0.473.1
