54c7279d9fab97e284704a471edb952d04ee460b
Cengiz Ilerler <cilerler@gmail.com>
2018-05-19 17:13:48 -0400

    Update build.ps1

--- tools/build.ps1
+++ tools/build.ps1
@@ -10,14 +10,15 @@ if(Test-Path ..\artifacts) {
 $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
-$version = @{ $true = $env:APPVEYOR_BUILD_VERSION; $false = ""}
+$version = @{ $true = $env:APPVEYOR_BUILD_VERSION.ToString(); $false = ""}
 
 echo "build: Version suffix is $suffix"
 echo "build: Version revision is $revision"
 echo "build: Version branch is $branch"
 echo "build: Version version is $version"
+Write-Host "Version: $env:APPVEYOR_BUILD_VERSION"
 
-echo "Attempting to pack file: burcin.nuspec"
+Write-Host "Attempting to pack file..."
 	& nuget pack ..\burcin.nuspec -NonInteractive -OutputDirectory ..\artifacts -Verbosity Detailed -version 1.0.473.1
     if($LASTEXITCODE -ne 0) { exit 1 }
 
