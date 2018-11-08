0c2aef7b3f425cdb3ec94c374e5f7784b9802788
Cengiz Ilerler <cilerler@gmail.com>
2018-11-08 13:41:15 -0500

    implemented variable name

--- test/Test.ps1
+++ test/Test.ps1
@@ -1,9 +1,10 @@
+$repository = "Bedia";
 nuget pack burcin.nuspec;
 dotnet new -i Burcin.Templates.CSharp.0.0.1.nupkg;
 Remove-Item .\Burcin.Templates.CSharp.0.0.1.nupkg;
-Remove-Item -Recurse -Force "Bedia";
-New-Item -ItemType "directory" -Name "Bedia";
-Set-Location "Bedia";
+Remove-Item -Recurse -Force $repository;
+New-Item -ItemType "directory" -Name $repository;
+Set-Location $repository;
 dotnet new burcin --ConsoleApplication --BackgroundService --WindowsService --HealthChecks --EntityFramework --TestFramework --DockerSupport --Swagger --VsCodeDirectory --GitHubTemplates --Cache "All" --DatabaseName "BediaDb" --Authors "Cengiz Ilerler" --RepositoryUrl "https://github.com/cilerler/bedia" --SkipRestore;
 dotnet new -u Burcin.Templates.CSharp.0.0.1;
 code .;
