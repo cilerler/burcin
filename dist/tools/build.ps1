03a15bd21ffa746e51cf2abd25783ea2dcc35a7a
Cengiz Ilerler <cilerler@gmail.com>
2018-11-19 08:31:38 -0500

    added DoxFx support

--- dist/tools/build.ps1
+++ dist/tools/build.ps1
@@ -1,8 +1,13 @@
 $buildNumber = "0.$((Get-Date).ToString("yyMM.dd.HHmm"))";
 dotnet build /p:BuildNumber=$buildNumber -c Release;
 dotnet pack /p:BuildNumber=$buildNumber -c Release -o "..\nupkgs" --no-build --include-symbols;
+
 #//#if (!WindowsService)
 dotnet publish /p:BuildNumber=$buildNumber -c Release;
 #//#else
 dotnet publish /p:BuildNumber=$buildNumber -c Release --runtime win7-x64;
+#//#endif
+
+#//#if (DocFx)
+docfx docs\docfx\docfx.json --serve
 #//#endif
