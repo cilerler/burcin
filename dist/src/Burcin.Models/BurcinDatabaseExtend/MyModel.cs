2d949a91c7c89a52f6d5a1bffa464ecc5b8b46da
Cengiz Ilerler <cilerler@gmail.com>
2020-04-19 00:53:51 -0400

    add ISoftDelete, IAuditable, IBaseentity

---
+++ dist/src/Burcin.Models/BurcinDatabaseExtend/MyModel.cs
@@ -0,0 +1,10 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Brainiac.Models.Burcin
+{
+    // public partial class MyModel : IBaseEntity, IAuditable, ISoftDelete
+    // {
+    // }
+}
