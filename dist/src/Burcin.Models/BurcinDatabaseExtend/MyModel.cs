f7e8cf3b58b0d0e0e25908509c326c0cbf8a5674
Cengiz Ilerler <cilerler@gmail.com>
2020-04-22 07:49:38 -0400

    add sample model extend

--- dist/src/Burcin.Models/BurcinDatabaseExtend/MyModel.cs
+++ dist/src/Burcin.Models/BurcinDatabaseExtend/MyModel.cs
@@ -1,10 +1,35 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Burcin.Models;
 
 namespace Brainiac.Models.Burcin
 {
-    // public partial class MyModel : IBaseEntity, IAuditable, ISoftDelete
-    // {
-    // }
+    public partial class MyModel : IBaseEntity, IAuditable, ISoftDelete
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public Guid RowGuid { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public byte[] UpdateVersion { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [Required]
+        [Column(TypeName = "datetime2(3)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [Required]
+        [Column(TypeName = "datetime2(3)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime ModifiedAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public bool SoftDelete { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
 }
