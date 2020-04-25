4182ebc3f4bb8f3634b23fdd10d37e5f1017b7c8
Cengiz Ilerler <cilerler@gmail.com>
2020-04-25 16:36:54 -0400

    specify context at migrations

--- dist/tools/EntityFramework/migrate.ps1
+++ dist/tools/EntityFramework/migrate.ps1
@@ -1,3 +1,3 @@
 Set-Location ".\src\Burcin.Api";
-dotnet ef migrations add initial --project ../Burcin.Migrations/;
-dotnet ef database update;
+dotnet ef migrations add initial --context BurcinDatabaseDbContext --project ../Burcin.Migrations/;
+dotnet ef database update --context BurcinDatabaseDbContext;
