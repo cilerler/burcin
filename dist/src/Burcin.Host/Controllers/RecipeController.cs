c1052fb9f9180898c698051c45d19aa737b6ba64
Cengiz Ilerler <cilerler@gmail.com>
2020-06-26 11:05:42 -0400

    fix `$counter=true&$top=0` issue

--- dist/src/Burcin.Host/Controllers/RecipeController.cs
+++ dist/src/Burcin.Host/Controllers/RecipeController.cs
@@ -115,7 +115,7 @@ namespace Burcin.Host.Controllers
 
             var query = _dbContext.Recipes.AsQueryable();
             var records = options.ApplyTo(query);
-            if (records == null || !records.Any())
+            if (records == null)
             {
                 return NotFound();
             }
