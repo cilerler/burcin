a986c061bc16104f50929d2386887f465f249e78
Cengiz Ilerler <cilerler@gmail.com>
2020-06-26 11:24:11 -0400

    improve test method

--- test/Test.ps1
+++ test/Test.ps1
@@ -1,12 +1,16 @@
-$repository = "Bedia";
+$repository = "issue_cilerler-bedia-1";
+$database = "WideWorldImporters";
+$datetime = $(get-date -Format "yyyyMMdd");
+$folderPath = "$env:userprofile\Source\local\$datetime\$repository";
+$originalLocation = Get-Location;
 nuget pack burcin.nuspec;
 dotnet new -i Burcin.Templates.CSharp.0.0.1.nupkg;
 Remove-Item .\Burcin.Templates.CSharp.0.0.1.nupkg;
-Remove-Item -Recurse -Force $repository;
-New-Item -ItemType "directory" -Name $repository;
-Set-Location $repository;
-dotnet new burcin --WebApiApplication --HealthChecks --Swagger --BlazorApplication --ConsoleApplication --WindowsService --BackgroundService --EntityFramework --DatabaseName $repository --TestFramework --DocFx --DockerSupport --NugetSourceGitHub --NugetSourceAzureDevOps --VsCodeDirectory --GitHubTemplates --Cache "All" --Authors "Cengiz Ilerler" --RepositoryUrl "https://github.com/cilerler/bedia" --SkipRestore;
+Remove-Item -Recurse -Force $folderPath;
+New-Item -ItemType "directory" -Path $folderPath -ErrorAction Ignore;
+Set-Location $folderPath
+dotnet new burcin --WebApiApplication --HealthChecks --Swagger --BlazorApplication --ConsoleApplication --WindowsService --BackgroundService --EntityFramework --DatabaseName $database --TestFramework --DocFx --DockerSupport --NugetSourceGitHub --NugetSourceAzureDevOps --VsCodeDirectory --GitHubTemplates --Cache "All" --Authors "Cengiz Ilerler" --RepositoryUrl "https://github.com/cilerler/$repository" --SkipRestore;
 dotnet new -u Burcin.Templates.CSharp.0.0.1;
 &".\tools\createSolutionFile.ps1";
 code .;
-Set-Location ..;
+Set-Location $originalLocation;
