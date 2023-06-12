2a70f7525bfd41a5585474ffb38d13dbbef1e579
Cengiz Ilerler <cilerler@gmail.com>
2023-06-11 22:46:06 -0400

    update (#236)

--- test/Test.ps1
+++ test/Test.ps1
@@ -1,16 +1,19 @@
+# Ensure that you navigate to the project's root folder before proceeding!
+# Set-Location "$env:userprofile\Source\burcin";
+# PowerShell.exe -ExecutionPolicy Bypass -File "$env:userprofile\Source\burcin\test\Test.ps1";
 $repository = "issue.cilerler_wwi_1";
 $database = "WideWorldImporters";
 $datetime = $(get-date -Format "yyyyMMdd");
 $folderPath = "$env:userprofile\Source\local\$datetime\$repository";
 $originalLocation = Get-Location;
 nuget pack burcin.nuspec;
-dotnet new -i Burcin.Templates.CSharp.0.0.1.nupkg;
+dotnet new install .\Burcin.Templates.CSharp.0.0.1.nupkg;
 Remove-Item .\Burcin.Templates.CSharp.0.0.1.nupkg;
 Remove-Item -Recurse -Force $folderPath;
 New-Item -ItemType "directory" -Path $folderPath -ErrorAction Ignore;
 Set-Location $folderPath
 dotnet new burcin --WebApiApplication --HealthChecks --Swagger --BlazorApplication --ConsoleApplication --OData --WindowsService --BackgroundService --EntityFramework --DatabaseName $database --TestFramework --DocFx --DockerSupport --SerilogSupport --NugetSourceGitHub --NugetSourceAzureDevOps --VsCodeDirectory --GitHubTemplates --Cache "All" --Authors "Cengiz Ilerler" --RepositoryUrl "https://github.com/cilerler/$repository" --SkipRestore;
-dotnet new -u Burcin.Templates.CSharp.0.0.1;
+dotnet new uninstall Burcin.Templates.CSharp;
 &".\tools\createSolutionFile.ps1";
 code .;
 Set-Location $originalLocation;
